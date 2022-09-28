0f9ecf98573958ddaf9fd43c66a0d5bb51ab8217
Jaewon Chang <hepachupa@gmail.com>
2022-09-28 13:34:59 +0900

    페이퍼 마리오 움직임 구현

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6f1f87862852cc64f6dc62e15f271c972c6279e033859b583ac30b596744e0de
+size 480
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3882793b56dc34a9fe6911e6e863c89e255e97313a7c666d5d1bf26687d73f37
+size 1079
